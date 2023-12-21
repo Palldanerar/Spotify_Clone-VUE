0edef80cc8efbfb1c47381f75f07f34e9f8a0d81
Palldanerar <alexkharin86@gmail.com>
2023-12-21 09:28:19 +0300

    delete links

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -14,46 +14,46 @@
         <div class="py-1.5"></div>
 
         <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/45/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/76/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/56/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/25/300/300"/>
-            <CategorySelect category="Podcasts" image="https://picsum.photos/id/103/300/300"/>
-            <CategorySelect category="Audiobooks" image="https://picsum.photos/id/244/300/300"/>
-            <CategorySelect category="Made For You" image="https://picsum.photos/id/202/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/101/300/300"/>
-            <CategorySelect category="New Release" image="https://picsum.photos/id/120/300/300"/>
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks"/>
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="Podcasts" />
+            <CategorySelect category="Audiobooks" />
+            <CategorySelect category="Made For You" />
+            <CategorySelect category="New Release" />
+            <CategorySelect category="New Release" />
         </div>
     </div>
 </template>
